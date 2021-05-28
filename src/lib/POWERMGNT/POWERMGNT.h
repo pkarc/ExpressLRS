105d2ca8ec157aa1d47073776ad874c38f2d48c7
Ivan Jaramillo <ivanchojara@gmail.com>
2021-05-28 16:27:05 -0500

    update RX to SLim+

--- src/lib/POWERMGNT/POWERMGNT.h
+++ src/lib/POWERMGNT/POWERMGNT.h
@@ -10,17 +10,11 @@
 #include "SX1280Driver.h"
 #endif
 
-<<<<<<< HEAD
-#ifdef TARGET_R9M_TX
-#ifdef R9M_UNLOCK_HIGHER_POWER
-#define MaxPower 7
-=======
 #if defined(TARGET_1000mW_MODULE) || \
     defined(TARGET_R9M_TX)        || \
     defined(TARGET_TX_ES915TX)
 #ifdef UNLOCK_HIGHER_POWER
-#define MaxPower PWR_1000mW
->>>>>>> 1.0.0-RC1
+#define MaxPower PWR_2000mW
 #else
 #define MaxPower PWR_250mW
 #endif
